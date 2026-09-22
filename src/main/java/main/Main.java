de56bc0acb2f52810617945c4c678844b8b7a29d
AcxesLo <steve.klein.98@web.de>
2026-09-22 17:57:29 +0200

    added mvn wrapper; updated pom.xml; updated README

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,6 +7,7 @@ public class Main {
         //TODO
         // -add categories and filter by those categories
         // -add budget feature
+        // -make it that description doesnt need to be in double quotes
 
     public static void main(String[] args) {
         ExpensesService.executeTracker();
