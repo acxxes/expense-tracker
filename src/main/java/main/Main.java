6eb9781cf5f27ee8964a732255b3f6404f2eac28
AcxesLo <steve.klein.98@web.de>
2026-09-22 18:00:44 +0200

    updated TODO in Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // -add categories and filter by those categories
         // -add budget feature
         // -make it that description doesnt need to be in double quotes
+        // -accept double amounts instead of int only
 
     public static void main(String[] args) {
         ExpensesService.executeTracker();
